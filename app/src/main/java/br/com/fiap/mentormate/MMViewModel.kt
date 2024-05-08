d61ea9de458ea9a63c324247b63a207c09553985
SantosRodrigo89 <rodrigo.v.santos89@gmail.com>
2024-05-08 15:18:25 -0300

    feat: add popup

--- app/src/main/java/br/com/fiap/mentormate/MMViewModel.kt
+++ app/src/main/java/br/com/fiap/mentormate/MMViewModel.kt
@@ -1,11 +1,14 @@
 package br.com.fiap.mentormate
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import br.com.fiap.mentormate.data.Event
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.lang.Exception
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,5 +19,15 @@ class MMViewModel @Inject constructor(
 ) : ViewModel() {
 
     val inProgress = mutableStateOf(false)
+    val popupNotification = mutableStateOf<Event<String>?>(null)
+
+    private fun handleException(exception: Exception? = null, customMessage: String = "") {
+        Log.e("TinderClone","Tinder Exception", exception)
+        exception?.printStackTrace()
+        val errorMsg = exception?.localizedMessage ?: ""
+        val message = if (customMessage.isEmpty()) errorMsg else "$customMessage: $errorMsg"
+        popupNotification.value = Event(message)
+        inProgress.value = false
+    }
 
 }
