a1d77bd8d31198399f8ed5bb21414d220283e1f7
SantosRodrigo89 <rodrigo.v.santos89@gmail.com>
2024-05-14 16:42:28 -0300

    refactor: data types

--- app/src/main/java/br/com/fiap/mentormate/data/DataTypes.kt
+++ app/src/main/java/br/com/fiap/mentormate/data/DataTypes.kt
@@ -7,7 +7,10 @@ data class UserData(
     var imageUrl: String? = "",
     var bio: String? = "",
     var gender: String? = "",
-    var genderPreference: String? = ""
+    var genderPreference: String? = "",
+    var swipesLeft: List<String> =  listOf(),
+    var swipesRight: List<String> =  listOf(),
+    var matches: List<String> =  listOf()
 ) {
     fun toMap() = mapOf(
         "userId" to userId,
@@ -16,6 +19,9 @@ data class UserData(
         "imageUrl" to imageUrl,
         "bio" to bio,
         "gender" to gender,
-        "genderPreference" to genderPreference
+        "genderPreference" to genderPreference,
+        "swipesLeft" to swipesLeft,
+        "swipesRight" to swipesRight,
+        "matches" to matches
     )
 }
