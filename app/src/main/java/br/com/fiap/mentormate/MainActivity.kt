542acf684e4cf803b516a806ea4c7816a1b5d07d
SantosRodrigo89 <rodrigo.v.santos89@gmail.com>
2024-05-21 09:30:34 -0300

    refactor: remove button

--- app/src/main/java/br/com/fiap/mentormate/MainActivity.kt
+++ app/src/main/java/br/com/fiap/mentormate/MainActivity.kt
@@ -54,7 +54,6 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     SwipeAppNavigation(this)
-                    MainScreen(this)
                 }
             }
         }
